70f008e10f53d666477c7ebb1017b34291b08f92
mhmdmydn <muhammadmayudin12@gmail.com>
2021-10-24 04:50:16 +0700

    Fixxxxxxxxxxxx

--- app/controller/BotControllers.js
+++ app/controller/BotControllers.js
@@ -2,6 +2,7 @@
 const moment = require('moment')
 const fs = require('fs')
 const ytdl = require('ytdl-core')
+const youtubedl = require('youtube-dl-exec')
 const Utils = require('./../utils/Utils')
 const apikey = process.env.PIXABAY_APIKEY || ''
 
@@ -142,35 +143,46 @@ exports.main = (bot) => {
     //command ytdl 
     bot.command('/ytdl', async (ctx) => {
         
-        const command = ctx.message.text;
-        const pecah = command.split(' ')
+        let message_id = ctx.message.message_id;
+        let args = ctx.update.message.text.split(' ');
+        let url = args[1];
+        let mention = `@${ctx.message.from.username}`;
+        var dq = "2160";
+        let allowed_qualities = ['144', '240', '360', '480', '720', '1080', '1440', '2160'];
+        if (!url.match(/^(?:https?:)?(?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]{7,15})(?:[\?&][a-zA-Z0-9\_-]+=[a-zA-Z0-9\_-]+)*(?:[&\/\#].*)?$/)) return ctx.reply("Enter a valid youtube url", { reply_to_message_id: message_id, parse_mode: 'Markdown' })
         
-        if (!pecah[1]) {
-            ctx.reply('Harap masukan url youtube!!!', {
-                "reply_to_message_id": ctx.message.message_id
+        if (args[2] && allowed_qualities.includes(args[2])) {
+            var dq = `${args[2]}`
+            ctx.reply("Processing your video with the chosen quality", { reply_to_message_id: message_id, parse_mode: 'Markdown' })
+        } else if (!args[2]) {
+            ctx.reply("Processing your video with max quality", { reply_to_message_id: message_id, parse_mode: 'Markdown' })
+        } else if (args[2] && !allowed_qualities.includes(args[2])) {
+            ctx.reply("Invalid quality settings chosen , video will be downloaded with highest possible quality", { reply_to_message_id: message_id, parse_mode: 'Markdown' })
+        }
+        if (ctx.message.from.username == undefined) {
+            mention = ctx.message.from.first_name
+        }
+        try {
+            youtubedl(url, {
+                format: `bestvideo[height<=${dq}]+bestaudio/best[height<=${dq}]`,
+                dumpSingleJson: true,
+                noWarnings: true,
+                noCallHome: true,
+                noCheckCertificate: true,
+                preferFreeFormats: true,
+                youtubeSkipDashManifest: true,
+            }).then(output =>
+                ctx.reply(`***Title: ${output.title} ***\n[Download Link](${output.requested_formats[0].url})\n***Video Requested By: [${mention}]***`, {
+                    reply_to_message_id: message_id,
+                    parse_mode: 'Markdown'
+                })
+            )
+        } catch (error) {
+            console.error(error);
+            ctx.reply("***Error occurred, Make sure your sent a correct URL***", {
+                reply_to_message_id: message_id,
+                parse_mode: 'Markdown'
             })
-        } else {
-            try {
-                const info = await ytdl.getInfo(pecah[1])
-
-                
-                const  format = await ytdl.chooseFormat(info.formats, { quality: 'lowestvideo' });
-                
-                if (format.hasVideo) {
-                    ctx.reply(JSON.stringify(format, null, 4), {
-                        "reply_to_message_id": ctx.message.message_id
-                    })
-                }
-                
-            } catch (error) {
-                console.log(error);
-                
-            }
-            
-            ctx.reply(format, {
-                "reply_to_message_id": ctx.message.message_id
-            })
-
         }
 
     })
